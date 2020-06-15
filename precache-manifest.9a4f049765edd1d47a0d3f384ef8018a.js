716b930af4c726480eb7ca81d6faefedaf81d8fd
Carter Klein <carterklein13@gmail.com>
2020-06-15 08:20:46 -0400

    Updates

---
+++ precache-manifest.9a4f049765edd1d47a0d3f384ef8018a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dddf3cfbf40ba9aed54a786bba95834",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db781384e7dc99dd1e93",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "6aad27899824aabc523c",
+    "url": "./static/js/2.8a11de54.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.8a11de54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db781384e7dc99dd1e93",
+    "url": "./static/js/main.9f6f3a14.chunk.js"
+  },
+  {
+    "revision": "f0952b0dfa059fbce29e",
+    "url": "./static/js/runtime-main.3134669e.js"
+  }
+]);
